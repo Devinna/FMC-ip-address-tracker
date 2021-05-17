98eef11a96bdcf3556e581db1159410a7c4a6969
Devinnakie <devinnakie@gmail.com>
2021-05-17 14:14:43 +0700

    First commit

---
+++ src/components/TheHeader.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <h2>IP Address Tracker</h2>
+    <v-btn color='primary'>Click Me</v-btn>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TheHeader',
+}
+</script>
